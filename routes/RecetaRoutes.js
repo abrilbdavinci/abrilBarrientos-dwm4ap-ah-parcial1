62ec7cff5a7334d3d5f9d50f5e97af9d3d9dc5eb
Abru <abrilpilarb@gmail.com>
2025-09-15 04:34:47 -0300

    aver que pasa

--- routes/RecetaRoutes.js
+++ routes/RecetaRoutes.js
@@ -6,13 +6,14 @@ const router = express.Router();
 
 // Recetas CRUD
 router.get("/", RecetaController.obtenerTodas);
+
+// Regiones (move these above "/:id")
+router.get("/region", RecetaController.obtenerTodasRegiones);
+router.get("/region/:id", RecetaController.obtenerPorIdRegion);
+
 router.get("/:id", RecetaController.obtenerPorId);
 router.post("/", RecetaController.crear);
 router.put("/:id", RecetaController.actualizar);
 router.delete("/:id", RecetaController.eliminar);
-
-// Regiones
-router.get("/region", RecetaController.obtenerTodasRegiones);
-router.get("/region/:id", RecetaController.obtenerPorIdRegion);
 
 export default router;
